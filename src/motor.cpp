b662b2ba9dfe88546a3f1f90d062cd4feab71a57
Error_Account <66193775+sarojlol@users.noreply.github.com>
2023-02-06 21:07:42 +0700

    modified:   src/motor.cpp

--- src/motor.cpp
+++ src/motor.cpp
@@ -100,12 +100,12 @@ int ultrasonic_distance(){
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(sc_trig, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(5);
   digitalWrite(sc_trig, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(sc_echo, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2;
+  distance = duration/29/2;
   // Prints the distance on the Serial Monitor
   return distance;
 }
